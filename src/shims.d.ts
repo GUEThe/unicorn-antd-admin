cf37d49ae3b81751b15ebce99b0421ef1d3b101d
何建钦 <1373883296@qq.com>
2020-12-29 09:17:53 +0800

    add ant-design-vue

--- src/shims.d.ts
+++ src/shims.d.ts
@@ -3,3 +3,11 @@ declare module '*.vue' {
   const _default: Component
   export default _default
 }
+
+
+declare module 'vue-router' {
+  interface RouteMeta {
+    // 权限
+    permission?: string[]
+  }
+}
